0203b1769f42f9eff9c20532d5042463129ef044
idv17cjramos <31974477+idv17cjramos@users.noreply.github.com>
2018-05-10 20:32:26 -0500

    Capitulo 2
    Drills y Ejercicios del Capitulo 2

---
+++ LibroStormtrooper/Cap 2 Excer/Main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+#include "Windows.h"
+
+using namespace std;
+
+void Ex1();
+void GoToBathroom();
+
+int main()
+{
+	Ex1();
+	
+	system("pause");
+	return 0;
+}
+
+void Ex1()
+{
+
+	cout << "Hello Programming" << endl;
+	cout << "Here we go!" << endl;
+
+}
+
+void GoToBathroom()
+{
+	cout << "-----Going to bathroom-----" << endl;
+	cout << "Stand up" << endl;
+	cout << "Turn 180 degrees" << endl;
+	cout << "Move the chair that is in front of you so that it is to your right side" << endl;
+	cout << "Take three forward steps" << endl;
+	cout << "Turn left" << endl;
+	cout << "Take 6 forward steps" << endl;
+	cout << "Turn right" << endl;
+	cout << "Reach the doorknob with your hand" << endl;
+	cout << "Twist the doorknob counter clockwise and pull towards you" << endl;
+	cout << "Let go the doorknob" << endl;
+	cout << "Once the door is completely open, take two steps forward and enter the bathroom" << endl;
+	cout << "Turn left, take 2 steps backwards and sit down" << endl;
+	cout << "You cand start minding your business" << endl;
+}
+
+void BakingMuffins()
+{
+
+	cout << "NOPE" << endl;
+
+	cout << "Pero si entiendo que es como la estructura hasica de un programa, das insturcciones, defines objetos y asi" << endl;
+}
+
+void Terms()
+{
+	cout << "//	"<< "Es un caracter reservado en c++ el cual le dice al compilador que va a ser un comentario, y que no lo lea como codigo"<<endl;
+	cout << "executble"<< "Es el programa en si, el codigo ya corriendo como una aplicacion"<<endl;
+	cout << "main"<< "Es al funcion principal en c++, se llama al principio y de ahi corre todo."<<endl;
+	cout << "() "<< "Dentro de una funcion de C++ es donde esta recibe parametros"<<endl;
+	cout << "<< "<< "Los corchetes "<<endl;
+	cout << "functin "<<  ""<<endl;
+	cout << "object"<<  ""<<endl;
+	cout << "code "<<  ""<<endl;
+	cout << "C++"<<  ""<<endl;
+	cout << "header"<<  ""<<endl;
+	cout << "output"<<  ""<<endl;
+	cout << "commen "<<  ""<<endl;
+	cout << "IDE "<<  ""<<endl;
+	cout << "progra "<<  ""<<endl;
+	cout << "compilr"<<  ""<<endl;
+	cout << "#inclue "<<  ""<<endl;
+	cout << "source"<<  ""<<endl;
+	cout << "code "<<  ""<<endl;
+	cout << "compil-tie eror"<<  ""<<endl;
+	cout << "librar "<<  ""<<endl;
+	cout << "statemnt "<<  ""<<endl;
+	cout << "cout "<<  ""<<endl;
+	cout << "linker"<<  ""<<endl;
+
+
+}
---
+++ LibroStormtrooper/Cap 2 Excercises/Main.cpp
@@ -0,0 +1,9 @@
+#include "StdLibfacilities.h"
+
+int main()
+{
+	cout << "Hello World" << endl;
+	keep_window_open();
+	return 0;
+
+}
